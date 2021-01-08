9a334539544b46512d1bca6cc9d2a238193f4f62
Prabhav Desai <desaipd1220@gmail.com>
2021-01-08 15:34:46 -0700

    connect database

--- app.js
+++ app.js
@@ -4,7 +4,14 @@ const mongoose = require('mongoose');
 
 const app = express();
 
+//DB Config
 
+const db = require('./config/keys').MongoURI;
+
+//Connect to Mongo
+mongoose.connect(db, {useNewUrlParser: true})
+    .then(() => console.log('MongoDB Connected...'))
+    .catch(err => console.log(err));
 
 // EJS Middleware
 app.use(expressLayouts);
