2c93fb5a44452cb9c2e6b6a9407917bedbdede4f
Prabhav Desai <desaipd1220@gmail.com>
2021-01-08 14:38:58 -0700

    set up server PORT

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
-const express =  require
+const express =  require('express');
+
+const app = express();
+
+const PORT = process.env.PORT || 5000; //5000 on local host
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`))
